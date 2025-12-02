4a508083da6a547527afbb91106735f5783dbed4
Jose Miguel . <mikejosemiguel25@gmail.com>
2025-12-01 22:00:35 -0600

    FLASK_HTML_BOOTSTRAP_SQL

---
+++ FLASK_HTML_CSS_BOOTSTRAP/cliente_forma.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import HiddenField, IntegerField, StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class ClienteForma(FlaskForm):
+    id = HiddenField('id')
+    nombre = StringField('Nombre', validators=[DataRequired()])
+    apellido = StringField('Apellido', validators=[DataRequired()])
+    membresia = IntegerField('Membresia', validators=[DataRequired()])
+    guardar = SubmitField('Guardar')
+
+
